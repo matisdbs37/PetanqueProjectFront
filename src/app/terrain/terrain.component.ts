23d16360bf9cf59b190a0f24f30f0f921cfc6da5
ChartonMatthieu <153715941+ChartonMatthieu@users.noreply.github.com>
2024-11-22 14:39:54 +0100

    terrain component

---
+++ src/app/terrain/terrain.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terrain',
+  standalone: true,
+  imports: [],
+  templateUrl: './terrain.component.html',
+  styleUrl: './terrain.component.css'
+})
+export class TerrainComponent {
+
+}
