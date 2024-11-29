72146e9843c6ffbdd5453f9d52a09cc530443667
emsamax <martin.neron@laposte.net>
2024-11-29 17:45:05 +0100

    frist version of mapp view

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit, OnInit{
+  //specify the map type from leaflet
+  private map! : L.Map;
 
+  constructor() {}
+
+  private async initMap(): Promise<void> {
+    const L = await import('leaflet');
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  async ngOnInit(): Promise<void> {
+    // Vérifier si on est dans un environnement client
+    if (typeof window !== 'undefined') {
+      const L = await import('leaflet'); // Import dynamique de Leaflet
+      this.initMap();
+    }
+  }
 }
